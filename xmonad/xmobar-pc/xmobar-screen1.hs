c856196035ed392904027436c1c5241330aff0ff
Wolfgang Mueller <vehk@kindofautomatic.de>
2013-04-28 22:47:44 +0200

    Remove vc in mplayer config.
    Get some padding for xmobar network plugin.

--- xmonad/xmobar-pc/xmobar-screen1.hs
+++ xmonad/xmobar-pc/xmobar-screen1.hs
@@ -5,10 +5,10 @@ Config {
     position = Top,
     lowerOnStart = True,
     commands = [ 
-        Run Network "enp5s0" ["-t", "<fc=#387BAB><rx>kb/s</fc> <fc=#005F87><tx>kb/s</fc>"] 15,
+        Run Network "enp5s0" ["-t", "<dev> <fc=#387BAB><rx>kb/s</fc> <fc=#005F87><tx>kb/s</fc>", "-w", "4"] 15,
         Run Memory [] 50,
         Run Date "%a %y-%m-%d %H:%M:%S %Z [%z] " "date" 10,
         Run StdinReader
     ],
-    template = " %StdinReader% }{ %enp5s0% <fc=#3F3F3F>|</fc> <fc=#B973FF>%memory%</fc> <fc=#3F3F3F>|</fc> <fc=#D0CFD0>%date%</fc>"
+    template = " %StdinReader% }{ <fc=#3F3F3F> %enp5s0% |</fc> <fc=#B973FF>%memory%</fc> <fc=#3F3F3F>|</fc> <fc=#D0CFD0>%date%</fc>"
 }
